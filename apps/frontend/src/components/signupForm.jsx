04fe2dfb0e0127d81371e58fda35b8fa8354eb28
nrj-kmr <mrneeraaj@gmail.com>
2024-07-23 16:03:53 +0530

    now able to push code from lab Computer

--- apps/frontend/src/components/signupForm.jsx
+++ apps/frontend/src/components/signupForm.jsx
@@ -1,5 +1,5 @@
-import axios from "axios"
-import { useEffect, useState } from "react";
+// import axios from "axios"
+// import { useEffect, useState } from "react";
 
 export default function SignupForm() {
     // Handle formData here and send it to the backend so that backend can handle and store the formData in the Database. 
@@ -7,22 +7,22 @@ export default function SignupForm() {
     // also do similar for login(check if user exists in db or note, if not then ask to signup displaying a proper message) And once done then route the user to their respective dashboards
 
     // here onwards handle loginForm Data
-    const [data, setData] = useState("")
-    const handleSignup = () => {
-        const data = {
+    // const [data, setData] = useState("")
+    // const handleSignup = () => {
+    //     const data = {
 
-        }
+    //     }
 
-        useEffect(() => {
-            axios.post('http://localhost:8000/user')
-                .then(response => {
-                    setData(response.data);
-                })
-                .catch(error => {
-                    console.error('There was an error fetching data!', error);
-                });
-        }, []);
-    } // handle the login/signup-Form Data here !!!
+    //     useEffect(() => {
+    //         axios.post('http://localhost:8000/user')
+    //             .then(response => {
+    //                 setData(response.data);
+    //             })
+    //             .catch(error => {
+    //                 console.error('There was an error fetching data!', error);
+    //             });
+    //     }, []);
+    // } // handle the login/signup-Form Data here !!!
 
     return (
         <div className="bg-slate-600 p-6 rounded-lg shadow-md">
@@ -35,7 +35,7 @@ export default function SignupForm() {
                     </div>
                     <div className="mb-4">
                         <label className="block text-sm font-medium">Email</label>
-                        <input type="email" placeholder="username" className="w-full p-2 mt-1 bg-slate-700 border border-gray-500 rounded" />
+                        <input type="email" placeholder="Email" className="w-full p-2 mt-1 bg-slate-700 border border-gray-500 rounded" />
                     </div>
                     <div className="mb-4">
                         <label className="block text-sm font-medium">Password</label>
@@ -46,7 +46,7 @@ export default function SignupForm() {
                         <label>isAdmin</label>
                     </div>
 
-                    <button type="submit" onClick={handleSignup()} className="w-full p-2 mt-5 bg-blue-600 rounded hover:bg-blue-700 transition-colors">Signup</button>
+                    {/* <button type="submit" onClick={handleSignup()} className="w-full p-2 mt-5 bg-blue-600 rounded hover:bg-blue-700 transition-colors">Signup</button> */}
                 </form>
             </div>
         </div>
