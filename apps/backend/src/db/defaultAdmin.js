02d63b347f9157f73203000dcdfa1b29320ab152
nrj-kmr <mrneeraaj@gmail.com>
2024-08-29 19:16:48 +0530

    feat: :sparkles: seed script to create a default admin and department
    this commit contains files, to create a default admin and department while setting up the project in a new machine

---
+++ apps/backend/src/db/defaultAdmin.js
@@ -0,0 +1,9 @@
+export const defaultAdmin = {
+     firstname: 'Admin',
+     lastname: 'User',
+     email: 'admin@example.com',
+     password: 'admin123',
+     department: 'default',
+     isAdmin: true,
+     role: 'manager',
+};
